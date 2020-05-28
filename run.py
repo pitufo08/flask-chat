09dcdcbd0e5414f419a83885e880c3ca59d55869
pitufo08 <camilo.vasquez82@gmail.com>
2020-05-28 14:34:01 +0200

    Initial commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flaks inmport Flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
    return "<h1>Hello There!</h1>"
 
-app.run(host=os.getenv('IP'), os.getenv('PORT')), debug=True)
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
